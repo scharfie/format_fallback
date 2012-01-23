d232573b2681d53a4e26617a266feec84524b8d4
Chris Scharf <scharfie@gmail.com>
2012-01-23 12:47:04 -0500

    don't need the debugger statement

--- lib/format_fallback.rb
+++ lib/format_fallback.rb
@@ -3,7 +3,6 @@ require "format_fallback/version"
 module ActionView
   class PathSet < Array
     def find_template_with_fallback(original_template_path, format = nil, html_fallback = true)
-      debugger
       find_template_without_fallback(original_template_path, format, html_fallback)
     rescue MissingTemplate => exception
       raise exception if format == :html
